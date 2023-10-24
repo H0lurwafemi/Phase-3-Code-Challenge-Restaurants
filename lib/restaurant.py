a0e9baa9818a87165ff330a591b843caf64f438b
unknown <stephenoluwafemi52@gmail.com>
2023-10-24 09:27:53 +0100

    Phase 3 Code Challenge Restaurants

---
+++ lib/restaurant.py
@@ -0,0 +1,41 @@
+from sqlalchemy import String, Integer, ForeignKey, Column, create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+
+    reviews = relationship('Review', back_populates='restaurant')
+    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+    reviews = relationship('Review', back_populates='customer')
+    restaurants = relationship('Restaurant', secondary='reviews', back_populates='customers')
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    star_rating = Column(Integer)
+
+    customer = relationship('Customer', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
+
+engine = create_engine('sqlite:///restaurants.db', echo=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
